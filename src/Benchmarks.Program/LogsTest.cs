1b8b67ecce9848fc3a808d04849ce0613d085504
Igor Jorge <igorjplino@gmail.com>
2022-10-23 11:25:14 +0100

    Created benchmark of microsoft logging

---
+++ src/Benchmarks.Program/LogsTest.cs
@@ -0,0 +1,29 @@
+﻿using BenchmarkDotNet.Attributes;
+using Microsoft.Extensions.Logging;
+
+[MemoryDiagnoser]
+public class LogsTest
+{
+    private const long _amountIterations = 1_000_000;
+    private const string _logMessage = "This a log message: {0} | {1}"; 
+    
+    private readonly ILoggerFactory _logMicrosoftFactory = LoggerFactory.Create(builder => 
+    {
+        builder.AddConsole().SetMinimumLevel(LogLevel.Warning);
+    });
+
+    private readonly ILogger _logMicrosoft;
+
+    public LogsTest()
+    {
+        _logMicrosoft = new Logger<LogsTest>(_logMicrosoftFactory);
+    }
+
+    public static int TestNumber => 1;
+
+    [Benchmark]
+    public void LogMicrosoft()
+    {
+        _logMicrosoft.LogInformation(_logMessage, 1, 2);
+    }
+}
---
+++ src/Benchmarks.Program/Program.cs
@@ -0,0 +1,28 @@
+﻿using BenchmarkDotNet.Running;
+
+Console.WriteLine("Hello, let's do some Benchmark tests!");
+
+int testNumber;
+
+do
+{
+    Console.WriteLine("Choose the test number you would like to execute:");
+    Console.WriteLine($"{LogsTest.TestNumber} - Logs");
+    
+    int.TryParse(Console.ReadLine(), out testNumber);
+    
+} while (!IsTestValid(testNumber));
+
+switch (testNumber)
+{
+    case 1:
+        BenchmarkRunner.Run<LogsTest>();
+        break;
+}
+
+bool IsTestValid(int testNumber)
+{
+    var validsTests = new int[] { LogsTest.TestNumber };
+
+    return validsTests.Any(o => o == testNumber);
+}
